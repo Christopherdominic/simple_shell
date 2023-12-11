3f4efedd83db700d9437280dc2d889691978c063
christopher dominic <chriseze0@gmail.com>
2023-12-10 16:13:27 -0800

    third commit

--- getline.c
+++ getline.c
@@ -8,43 +8,41 @@
 */
 int custom_getline(container_of_program *data)
 {
-	char buff[BUFFER_SIZE] = {'\0'};
-	static char *array_commands[10] = {NULL};
-	static char array_operators[10] = {'\0'};
+	char buffer[BUFFER_SIZE] = {'\0'};
+	static char *array_size[10] = {NULL};
+	static char array_operator[10] = {'\0'};
 	ssize_t bytes_read, i = 0;
 
-	/* check if doesnot exist more commands in the array */
-	/* and checks the logical operators */
-	if (!array_commands[0] || (array_operators[0] == '&' && errno != 0) ||
-		(array_operators[0] == '|' && errno == 0))
+
+	if (!array_size[0] || (array_operator[0] == '&' && errno != 0) ||
+		(array_operator[0] == '|' && errno == 0))
 	{
-		/*free the memory allocated in the array if it exists */
-		for (i = 0; array_commands[i]; i++)
+	
+		for (i = 0; array_size[i]; i++)
 		{
-			free(array_commands[i]);
-			array_commands[i] = NULL;
+			free(array_size[i]);
+			array_size[i] = NULL;
 		}
 
-		/* read from the file descriptor int to buff */
-		bytes_read = read(data->custom_fd, &buff, BUFFER_SIZE - 1);
+		
+		bytes_read = read(data->custom_fd, &buffer, BUFFER_SIZE - 1);
 		if (bytes_read == 0)
 			return (-1);
 
-		/* split lines for \n or ; */
+		
 		i = 0;
 		do {
-			array_commands[i] = str_duplicate(_strtok(i ? NULL : buff, "\n;"));
-			/*checks and split for && and || operators*/
-			i = check_logic_ops(array_commands, i, array_operators);
-		} while (array_commands[i++]);
+			array_size[i] = str_duplicate(_strtok(i ? NULL : buffer, "\n;"));
+
+			i = check_logic_ops(array_size, i, array_operator);
+		} while (array_size[i++]);
 	}
 
-	/*obtains the next command (command 0) and remove it for the array*/
-	data->custom_input_line = array_commands[0];
-	for (i = 0; array_commands[i]; i++)
+	data->custom_input_line = array_size[0];
+	for (i = 0; array_size[i]; i++)
 	{
-		array_commands[i] = array_commands[i + 1];
-		array_operators[i] = array_operators[i + 1];
+		array_size[i] = array_size[i + 1];
+		array_operator[i] = array_operator[i + 1];
 	}
 
 	return (str_length(data->custom_input_line));
@@ -64,12 +62,11 @@ int check_logic_ops(char *array_commands[], int i, char array_operators[])
 	char *temp = NULL;
 	int j;
 
-	/* checks for the & char in the command line*/
 	for (j = 0; array_commands[i] != NULL  && array_commands[i][j]; j++)
 	{
 		if (array_commands[i][j] == '&' && array_commands[i][j + 1] == '&')
 		{
-			/* split the line when chars && was found */
+
 			temp = array_commands[i];
 			array_commands[i][j] = '\0';
 			array_commands[i] = str_duplicate(array_commands[i]);
@@ -81,7 +78,7 @@ int check_logic_ops(char *array_commands[], int i, char array_operators[])
 		}
 		if (array_commands[i][j] == '|' && array_commands[i][j + 1] == '|')
 		{
-			/* split the line when chars || was found */
+
 			temp = array_commands[i];
 			array_commands[i][j] = '\0';
 			array_commands[i] = str_duplicate(array_commands[i]);
--- tokenizer.c
+++ tokenizer.c
@@ -6,37 +6,40 @@
  */
 void tokenize(container_of_program *data)
 {
-	char *delimiter = " \t";
-	int i, j, counter = 2, length;
+	char *del = " \t";
+	int i; 
+	int j;
+	int count = 2;
+	int len;
 
-	length = str_length(data->custom_input_line);
-	if (length)
+	len = str_length(data->custom_input_line);
+	if (len)
 	{
-		if (data->custom_input_line[length - 1] == '\n')
-			data->custom_input_line[length - 1] = '\0';
+		if (data->custom_input_line[len - 1] == '\n')
+			data->custom_input_line[len - 1] = '\0';
 	}
 
 	for (i = 0; data->custom_input_line[i]; i++)
 	{
-		for (j = 0; delimiter[j]; j++)
+		for (j = 0; del[j]; j++)
 		{
-			if (data->custom_input_line[i] == delimiter[j])
-				counter++;
+			if (data->custom_input_line[i] == del[j])
+				count++;
 		}
 	}
 
-	data->custom_tokens = malloc(counter * sizeof(char *));
+	data->custom_tokens = malloc(count * sizeof(char *));
 	if (data->custom_tokens == NULL)
 	{
 		perror(data->custom_program_name);
 		exit(errno);
 	}
 	i = 0;
-	data->custom_tokens[i] = str_duplicate(_strtok(data->custom_input_line, delimiter));
+	data->custom_tokens[i] = str_duplicate(_strtok(data->custom_input_line, del));
 	data->custom_command_name = str_duplicate(data->custom_tokens[0]);
 	while (data->custom_tokens[i++])
 	{
-		data->custom_tokens[i] = str_duplicate(_strtok(NULL, delimiter));
+		data->custom_tokens[i] = str_duplicate(_strtok(NULL, del));
 	}
 }
 
